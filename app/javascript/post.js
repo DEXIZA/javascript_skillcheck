a45ab9f94919d28393ce685eecbb04677f847654
DEXIZA <dexizaia@gmail.com>
2021-06-07 18:11:18 +0900

    めも２

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,10 +1,14 @@
 function post (){
   const submit = document.getElementById("submit_btn");
   submit.addEventListener("click", (e) => {
-    console.log(document.getElementById("new_article"));
-    // htmlの　post,id: "new_article"から引用している
     e.preventDefault();
+    // 重複防止用
   });
  };
  
  window.addEventListener('load', post);
+
+
+//  投稿の確認用メモ
+//  console.log(document.getElementById("new_article"));
+//  // htmlの　post,id: "new_article"から引用している
